06da963fb241427fec35492b4bffaaab3afd9b1f
[파인원]최태민 <xoals628@pineone.com>
2024-01-09 10:49:31 +0900

    라우팅 기초설정, 레이아웃 기초 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import routes from "./system/router";
+import { URL_LOGIN, URL_NOT_FOUND } from "system/URL";
+import Layout from "components/Layout";
 
 function App() {
+  // 로그인 여부 권한 체크 -> 추후 전역 상태관리로 이동해야 함
+  const checkAuth = () => {
+    return true;
+    // return false;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          {routes.map((route, index) => {
+            const { path, component: Component, exact, isPrivate } = route;
+
+            return (
+              <Route
+                key={index}
+                path={path}
+                exact={exact}
+                render={(props) =>
+                  // 로그인 여부에 따라 페이지를 렌더링
+                  isPrivate ? (
+                    checkAuth() ? (
+                      <Layout>
+                        <Component {...props} />
+                      </Layout>
+                    ) : (
+                      // 권한이 없는 경우 로그인 페이지로 리다이렉트
+                      <Redirect to={URL_LOGIN} />
+                    )
+                  ) : (
+                    <Component {...props} />
+                  )
+                }
+              />
+            );
+          })}
+          <Route
+            path="*"
+            render={(props) => <Redirect to={URL_NOT_FOUND} {...props} />}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
