4e1e4d90e34f5b9cf17a9aa19ad05b1ed8b97f4d
[파인원]최태민 <xoals628@pineone.com>
2024-01-18 16:47:59 +0900

    layout className, id추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
-const Layout = ({ children = () => <></>, CustomHeader }) => {
+const Layout = ({ id, className, children = () => <></>, CustomHeader }) => {
   const isHeader = !!CustomHeader;
 
   return (
-    <>
+    <div className={className} id={id}>
       {isHeader ? <CustomHeader /> : <Header />}
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
