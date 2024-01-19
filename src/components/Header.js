0c715eafc31f320a08233d0ff1dece9195554454
Heejineee <heejinee6@gmail.com>
2024-01-19 13:52:11 +0900

    [정희진] 메인 헤더 스크롤 그림자, 메뉴 오픈/숨김 이벤트 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,66 @@
 import { BASENAME, URL_HOME, URL_SEARCH } from "system/URL";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 60) {
+      setIsScroll(true);
+    } else {
+      setIsScroll(false);
+    }
+  };
+
   return (
-    <header id="mainHeader">
-      <Link to={URL_HOME} className="logo">
-        <img src={`${BASENAME}/assets/img/logo.png`} alt="로고" />
-      </Link>
-      <div className="icons">
-        <Link to={URL_SEARCH} id="search-info" className="icon">
-          <img src={`${BASENAME}/assets/icon/search.svg`} alt="검색" />
+    <>
+      <header id="mainHeader" className={`${isScroll ? "is-scroll" : ""}`}>
+        <Link to={URL_HOME} className="logo">
+          <img src={`${BASENAME}/assets/img/logo.png`} alt="로고" />
         </Link>
-        <span id="menu-icon" className="icon">
-          <img
-            src={`${BASENAME}/assets/icon/menu.svg`}
-            className="menu"
-            alt="메뉴"
-          />
-        </span>
-      </div>
-    </header>
+        <div className="icons">
+          <Link to={URL_SEARCH} id="search-info" className="icon">
+            <img src={`${BASENAME}/assets/icon/search.svg`} alt="검색" />
+          </Link>
+          <span
+            id="menu-icon"
+            className="icon"
+            onClick={() => {
+              setIsOpenMenu(!isOpenMenu);
+            }}
+          >
+            <img
+              src={`${BASENAME}/assets/icon/menu.svg`}
+              className="menu"
+              alt="메뉴"
+            />
+          </span>
+        </div>
+      </header>
+      <aside
+        id="menu"
+        className={`dropdown-menu ${isOpenMenu ? "is-open" : ""}`}
+      >
+        <Link to={URL_SEARCH} className="dropdown-item">
+          검색
+        </Link>
+        <a href="#" className="dropdown-item">
+          로그인 아이디 : 양태욱멋있어
+        </a>
+        <a href="#" className="dropdown-item">
+          로그 아웃
+        </a>
+      </aside>
+    </>
   );
 };
 
