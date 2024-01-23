c0075cb9a1648caafc27ee3beead075de1af3bb2
YangTaeWook <didxodnr0769@gmail.com>
2024-01-23 17:29:19 +0900

    [양태욱][Server] Fix : 속성 오류 수정

--- doc/server/DTO/UserDTO.js
+++ doc/server/DTO/UserDTO.js
@@ -1,7 +1,7 @@
 /**
  * UserDTO
  * @class UserDTO
- * @property {String} id - 사용자 고유 ID
+ * @property {Number} id - 사용자 고유 ID
  * @property {String} nickname - 사용자 닉네임
  * @property {Number} score - 사용자 점수
  * @property {String} profileImage - 사용자 프로필 이미지
