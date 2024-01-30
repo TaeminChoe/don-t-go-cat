41c57743fc258fdd6205578d033ca7b5dcc6af34
[파인원]최태민 <xoals628@pineone.com>
2024-01-30 13:43:12 +0900

    mock 코드 제거

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,15 +1,7 @@
-import { apiTest } from "utils/api";
-
 import Layout from "components/Layout";
 import ProductList from "components/ProductList";
-import Mock from "../../Mock.json";
 
 const HomePage = () => {
-  const handleClick = () => {
-    apiTest().then(console.log).catch(console.error);
-    // apiTest({ useLoading: false }).then(console.log).catch(console.error);
-  };
-
   return (
     <Layout>
       <ProductList />
