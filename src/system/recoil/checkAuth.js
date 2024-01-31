66d605ef2b42adc9c87b38db4e1e84e86e3e37fa
ParkHee-yeon <heeyun8662@naver.com>
2024-01-31 16:34:39 +0900

    [박희연] 로그인 기능 로직 수정

--- src/system/recoil/checkAuth.js
+++ src/system/recoil/checkAuth.js
@@ -1,10 +1,5 @@
 import { atom } from "recoil";
 
-export const checkAuthState = atom({
-  key: "checkAuthState",
-  default: false,
-});
-
 export const userInfoAtom = atom({
   key: "userInfoAtom",
   default: "",
