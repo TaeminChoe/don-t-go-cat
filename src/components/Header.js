227d4040f831b608f8a8e3348175d2ccf138ce59
YangTaeWook <didxodnr0769@gmail.com>
2024-02-02 11:34:55 +0900

    [양태욱][client][홈] 기능 개선 - 헤더 > 메뉴 > 로그인 아이디 표출 , 코드 개선

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,57 @@
-import { BASENAME, URL_HOME, URL_LOGIN, URL_SEARCH } from "system/URL";
+import { BASENAME, URL_HOME, URL_SEARCH } from "system/URL";
 import { Link } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import { userInfoAtom } from "system/recoil/checkAuth";
-import { useResetRecoilState } from "recoil";
+import { useRecoilValue, useResetRecoilState } from "recoil";
 
 const Header = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
   const [isScroll, setIsScroll] = useState(false);
-  const resetUserInfo = useResetRecoilState(userInfoAtom);
   const menuRef = useRef(null);
+  // 전역 상태의 사용자 정보 초기화
+  const resetUserInfo = useResetRecoilState(userInfoAtom);
+  // 전역 상태에서 가져온 사용자 정보
+  const { nickname = "" } = useRecoilValue(userInfoAtom);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleScroll = () => {
-    if (window.scrollY >= 60) {
-      setIsScroll(true);
-    } else {
-      setIsScroll(false);
-    }
-  };
-
-  const logout = () => {
-    setIsOpenMenu(!isOpenMenu);
-    // storage에 담겨져 있는 사용자 정보 삭제
-    sessionStorage.removeItem("userInfo");
-    resetUserInfo();
-  };
-
-  // 메뉴 열린 상태에서 리스트 스크롤시 메뉴 닫기
-  useEffect(() => {
-    function outsideCLick(event) {
+    /** 메뉴 닫힘 상태에서 스크롤 이벤트 : 다른 화면 클릭시 메뉴 닫기 */
+    const handleMenuClose = (event) => {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
         setIsOpenMenu(false);
       }
-    }
-    document.addEventListener("scroll", outsideCLick);
-    return () => {
-      document.removeEventListener("scroll", outsideCLick);
     };
-  }, [menuRef]);
+
+    /** 스크롤 상태에 따른 상태 변경 */
+    const handleScroll = () => {
+      if (window.scrollY >= 60) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    };
+    // 스크롤 관련 이벤트 추가
+    document.addEventListener("scroll", handleMenuClose);
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      // 컴포넌트 제거 시 스크롤 관련 이벤트 제거
+      document.removeEventListener("scroll", handleMenuClose);
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  /** 로그아웃 이벤트 : 사용자 관련 데이터 제거 및 전파 */
+  const logout = () => {
+    setIsOpenMenu(!isOpenMenu);
+    sessionStorage.removeItem("userInfo");
+    resetUserInfo();
+  };
 
   return (
     <>
       <header
         id="mainHeader"
-        className={`${isScroll ? "is-scroll" : ""}`}
+        // 스크롤 상태이면서 메뉴가 닫힌 상태일 때 Shadow UI 추가
+        className={`${isScroll && !isOpenMenu ? "is-scroll" : ""}`}
         ref={menuRef}
       >
         <Link to={URL_HOME} className="logo">
@@ -78,12 +80,15 @@ const Header = () => {
         id="menu"
         className={`dropdown-menu ${isOpenMenu ? "is-open" : ""}`}
       >
+        <div className="dropdown-item" onClick={() => {}}>
+          <span style={{ color: "#8c35fe", fontWeight: "bold" }}>
+            {nickname}
+          </span>
+          님 환영합니다.
+        </div>
         <Link to={URL_SEARCH} className="dropdown-item">
           검색
         </Link>
-        <a href="#" className="dropdown-item">
-          로그인 아이디 : 양태욱멋있어
-        </a>
         <div className="dropdown-item" onClick={logout}>
           로그 아웃
         </div>
